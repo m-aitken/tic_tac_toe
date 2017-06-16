d7abc24a4a9ea8a4f301894d9d1c162bd2c9275b
Matt Aitken <matt.aitken@gmail.com>
2017-06-16 10:19:43 +1000

    js comments

--- README.md
+++ README.md
@@ -8,18 +8,18 @@ As part of GA's Web Development Immersive course, the first project deliverable 
 
 # Requirements
 
-*The game must be rendered in a browser
-*Logic to show player turn switching
-*Logic to show a player winning, and a visual display to show which player won
-*Separate HTML, CSS and JavaScript files
+* The game must be rendered in a browser
+* Logic to show player turn switching
+* Logic to show a player winning, and a visual display to show which player won
+* Separate HTML, CSS and JavaScript files
 
 ---
 
 # Game Instructions
 
 Two players take turns clicking in the grid, assigning a marker, with Player One beginning first.
-*Player One marker: " X "
-*Player Two marker: " O "
+* Player One marker: " X "
+* Player Two marker: " O "
 
 The first player to successfully place 3 markers in a line, in any direction, wins the game. 
 If the game grid is filled with no winner, the game can be reset and started again by clicking the 'coin' icon to the left.
--- game.js
+++ game.js
@@ -1,67 +1,60 @@
 var player1 = "X";
 var player2 = "O";
 var boxes = document.querySelectorAll('.box');
-var currentPlayer = player1;
+var currentPlayer = player1; // game begins with Player One
 var displayPlayer = document.querySelector('span');
-var gridArray = [0,0,0,0,0,0,0,0,0]; // change element to X / O
+var gridArray = [0,0,0,0,0,0,0,0,0]; // store  X / O changes in array
 
-var switchPlayer = function () {
-  if (currentPlayer === player1) {
-    currentPlayer = player2;
+var switchPlayer = function () {      
+  if (currentPlayer === player1) {    
+    currentPlayer = player2;                  // swtich current player marker
   } else if (currentPlayer === player2) {
     currentPlayer = player1;
   }
-  displayPlayer.textContent = currentPlayer;
+  displayPlayer.textContent = currentPlayer;    // display new current player
 };
 
-var boardReset = function() {
-  gridArray = [0,0,0,0,0,0,0,0,0];
-  winner.textContent = "";
-  for (i=0; i<boxes.length; i++) {
+var boardReset = function() {       // reset board state
+  gridArray = [0,0,0,0,0,0,0,0,0];    // set logic array to original element values 
+  winner.textContent = "";            // clear any winner text
+  for (i=0; i<boxes.length; i++) {    // iterate through all box class, clear markers
     boxes[i] = boxes[i].textContent = ""
   }
   if (currentPlayer === player2) {
-    displayPlayer.textContent = player1;
+    displayPlayer.textContent = player1;    // reset to player1
   }
 };
 
-var boxChange = function() {
-  if (event.target.textContent === "") {
-  event.target.textContent = currentPlayer;
+var boxChange = function() {          // user action in grid box
+  if (event.target.textContent === "") {    // if box is empty
+  event.target.textContent = currentPlayer; // insert current player marker
   }
-  var targetInd = event.target.id.slice(-1);
-  gridArray[targetInd] = currentPlayer;
-  winCheck();
-  switchPlayer();
-  fullBoard();
+  var targetInd = event.target.id.slice(-1);  // find target box id
+  gridArray[targetInd] = currentPlayer;       // apply box id to game array as marker value(X/O)
+  winCheck();                   // call win condition check
+  switchPlayer();               // switch player
+  fullBoard();                  // call full board check
 };
 
-// full board/tied game  "start new game?"
-
-var fullBoard = function() {
-var fullArray = [];
-  for (i=0; i<gridArray.length; i++) {
+var fullBoard = function() {            // tied game/full board check
+var fullArray = [];                     // empty logic array
+  for (i=0; i<gridArray.length; i++) {  // iterate through game logic array
     if (gridArray[i] === "X" || gridArray[i] === "O") {
-      fullArray.push(gridArray[i]);
+      fullArray.push(gridArray[i]);     // if array has X/O push into empty array
     };
-    if (fullArray.length === 9) {
-      winner.textContent = "Start new game?"; // change out for game over image
+    if (fullArray.length === 9) {       // if empty array length is max(9 boxes)
+      winner.textContent = "Start new game?"; // display prompt for new game
     };
   };
-  fullArray = [];
+  fullArray = [];     // restore empty array state
 };
-
+// future improvement: change out for game over image
 
 var winner = document.querySelector('.winner');
-var winCheck = function () {
-// workings for clearer win conditions + game over image
+var winCheck = function () {        // check possible win conditions and display winner
 
-// if (i=0; i<gridArray.length; i++)
-//   gridArray[i]
-
-// if array indices === true for "x" or "o"
   if (gridArray[0] === "X" && gridArray[1] === "X" && gridArray[2] === "X") {
-    winner.textContent = "Player One wins!";
+    return winner.textContent = "Player One wins!";
   } else if (gridArray[0] === "O" && gridArray[1] === "O" && gridArray[2] === "O") {
     winner.textContent = "Player Two wins!";
     } else if (gridArray[3] === "X" && gridArray[4] === "X" && gridArray[5] === "X") {
@@ -94,13 +87,14 @@ var winCheck = function () {
       winner.textContent = "Player Two wins!";
     }
 };
+// future improvement: clearer win conditions + game over image
 
 var coin = document.querySelector('img');
-coin.addEventListener('click', boardReset);
+coin.addEventListener('click', boardReset);     // listen for click on coin img, call clear board function
 
 var grid = document.querySelector('#grid');
-grid.addEventListener('click', function(event) {
-  if (event.target.className === 'box') {
+grid.addEventListener('click', function(event) {  // listen for click in game grid, on a box class target, 
+  if (event.target.className === 'box') {         // call player maker function
     boxChange(event);
 
   }
