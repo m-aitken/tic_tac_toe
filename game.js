b554e2be154d4fcec1135f46123f255e78cebca1
Matt Aitken <matt.aitken@gmail.com>
2017-06-16 12:19:16 +1000

    errorfix

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var player1 = "X";
-var player2 = "O";
+var player1 = 1;
+var player2 = 2;
 var boxes = document.querySelectorAll('.box');
 var currentPlayer = player1; // game begins with Player One
 var displayPlayer = document.querySelector('span');
@@ -41,9 +41,9 @@ var boxChange = function() {          // user action in grid box
 var addImg = function () {
   var img = document.createElement("img");      // create image tag
   event.target.appendChild(img)                 // append image tag to click target
-  if (event.target.textContent === "X") {               
+  if (event.target.textContent === "1") {               
     img.setAttribute("src", "images/asteroid.gif"); // set image for player1 marker
-  } else if (event.target.textContent === "O") {
+  } else if (event.target.textContent === "2") {
     img.setAttribute("src", "images/rocket.gif");   // set image for player2 marker
   }
 };
@@ -51,7 +51,7 @@ var addImg = function () {
 var fullBoard = function() {            // tied game/full board check
 var fullArray = [];                     // empty logic array
   for (i=0; i<gridArray.length; i++) {  // iterate through game logic array
-    if (gridArray[i] === "X" || gridArray[i] === "O") {
+    if (gridArray[i] === 1 || gridArray[i] === 2) {
       fullArray.push(gridArray[i]);     // if array has X/O push into empty array
     };
     if (fullArray.length === 9) {       // if empty array length is max(9 boxes)
@@ -65,38 +65,38 @@ var fullArray = [];                     // empty logic array
 var winner = document.querySelector('.winner');
 var winCheck = function () {        // check possible win conditions and display winner
 
-  if (gridArray[0] === "X" && gridArray[1] === "X" && gridArray[2] === "X") {
+  if (gridArray[0] === 1 && gridArray[1] === 1 && gridArray[2] === 1) {
     return winner.textContent = "Player One wins!";
-  } else if (gridArray[0] === "O" && gridArray[1] === "O" && gridArray[2] === "O") {
-    winner.textContent = "Player Two wins!";
-    } else if (gridArray[3] === "X" && gridArray[4] === "X" && gridArray[5] === "X") {
-      winner.textContent = "Player One wins!";
-    } else if (gridArray[3] === "O" && gridArray[4] === "O" && gridArray[5] === "O") {
-      winner.textContent = "Player Two wins!";
-  } else if (gridArray[6] === "X" && gridArray[7] === "X" && gridArray[8] === "X") {
-    winner.textContent = "Player One wins!";
-  } else if (gridArray[6] === "O" && gridArray[7] === "O" && gridArray[8] === "O") {
-    winner.textContent = "Player Two wins!";
-    } else if (gridArray[0] === "X" && gridArray[3] === "X" && gridArray[6] === "X") {
-      winner.textContent = "Player One wins!";
-    } else if (gridArray[0] === "O" && gridArray[3] === "O" && gridArray[6] === "O") {
-      winner.textContent = "Player Two wins!";
-  } else if (gridArray[1] === "X" && gridArray[4] === "X" && gridArray[7] === "X") {
-    winner.textContent = "Player One wins!";
-  } else if (gridArray[1] === "O" && gridArray[4] === "O" && gridArray[7] === "O") {
-    winner.textContent = "Player Two wins!";
-    } else if (gridArray[2] === "X" && gridArray[5] === "X" && gridArray[8] === "X") {
-        winner.textContent = "Player One wins!";
-    } else if (gridArray[2] === "O" && gridArray[5] === "O" && gridArray[8] === "O") {
-        winner.textContent = "Player Two wins!";
-  } else if (gridArray[0] === "X" && gridArray[4] === "X" && gridArray[8] === "X") {
-      winner.textContent = "Player One wins!";
-  } else if (gridArray[0] === "O" && gridArray[4] === "O" && gridArray[8] === "O") {
-    winner.textContent = "Player Two wins!";
-    } else if (gridArray[2] === "X" && gridArray[4] === "X" && gridArray[6] === "X") {
-      winner.textContent = "Player One wins!";
-    } else if (gridArray[2] === "O" && gridArray[4] === "O" && gridArray[6] === "O") {
-      winner.textContent = "Player Two wins!";
+  } else if (gridArray[0] === 2 && gridArray[1] === 2 && gridArray[2] === 2) {
+    return winner.textContent = "Player Two wins!";
+    } else if (gridArray[3] === 1 && gridArray[4] === 1 && gridArray[5] === 1) {
+      return winner.textContent = "Player One wins!";
+    } else if (gridArray[3] === 2 && gridArray[4] === 2 && gridArray[5] === 2) {
+      return winner.textContent = "Player Two wins!";
+  } else if (gridArray[6] === 1 && gridArray[7] === 1 && gridArray[8] === 1) {
+    return winner.textContent = "Player One wins!";
+  } else if (gridArray[6] === 2 && gridArray[7] === 2 && gridArray[8] === 2) {
+    return winner.textContent = "Player Two wins!";
+    } else if (gridArray[0] === 1 && gridArray[3] === 1 && gridArray[6] === 1) {
+      return winner.textContent = "Player One wins!";
+    } else if (gridArray[0] === 2 && gridArray[3] === 2 && gridArray[6] === 2) {
+      return winner.textContent = "Player Two wins!";
+  } else if (gridArray[1] === 1 && gridArray[4] === 1 && gridArray[7] === 1) {
+    return winner.textContent = "Player One wins!";
+  } else if (gridArray[1] === 2 && gridArray[4] === 2 && gridArray[7] === 2) {
+    return winner.textContent = "Player Two wins!";
+    } else if (gridArray[2] === 1 && gridArray[5] === 1 && gridArray[8] === 1) {
+        return winner.textContent = "Player One wins!";
+    } else if (gridArray[2] === 2 && gridArray[5] === 2 && gridArray[8] === 2) {
+        return winner.textContent = "Player Two wins!";
+  } else if (gridArray[0] === 1 && gridArray[4] === 1 && gridArray[8] === 1) {
+      return winner.textContent = "Player One wins!";
+  } else if (gridArray[0] === 2 && gridArray[4] === 2 && gridArray[8] === 2) {
+    return winner.textContent = "Player Two wins!";
+    } else if (gridArray[2] === 1 && gridArray[4] === 1 && gridArray[6] === 1) {
+      return winner.textContent = "Player One wins!";
+    } else if (gridArray[2] === 2 && gridArray[4] === 2 && gridArray[6] === 2) {
+      return winner.textContent = "Player Two wins!";
     }
 };
 // future improvement: clearer win conditions + game over image
